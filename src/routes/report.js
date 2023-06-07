84b5e615c1da11b2af4f6fb9da14757341894685
Nephthali Salam <snephthali@gmail.com>
2023-06-07 12:44:48 +0200

    update getAllUserReportLength route

--- src/routes/report.js
+++ src/routes/report.js
@@ -7,7 +7,7 @@ router.post('/add-report', verifyToken.verifyToken, report.addReport)
 router.post('/period-reports', verifyToken.verifyToken, report.getReportForaPeriod)
 router.get('/all-reports', verifyToken.verifyToken, report.getAllReports)
 router.get('/user-reports', verifyToken.verifyToken, report.getAllUserReports)
-router.get('/user-reportsLength', verifyToken.verifyToken, report.getAllUserReports)
+router.get('/user-reportsLength', verifyToken.verifyToken, report.getAllUserReportLength)
 router.get('/single-report/:id', verifyToken.verifyToken, report.getSingleReport)
 
 module.exports = router
